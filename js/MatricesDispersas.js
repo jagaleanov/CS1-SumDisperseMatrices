784297c2eaca711f1dcf53360ae8a4970a13835b
jagaleanov <54491111+jagaleanov@users.noreply.github.com>
2020-07-19 13:06:43 -0500

    Update MatricesDispersas.js

--- js/MatricesDispersas.js
+++ js/MatricesDispersas.js
@@ -2,10 +2,9 @@
 
 //CLASES
 class NodeCol {
-
-    //next
-    //down
-    //col
+    next;
+    down;
+    col;
 
     constructor(col) {
         this.col = col;
@@ -15,10 +14,9 @@ class NodeCol {
 }
 
 class NodeRow {
-
-    //next;
-    //row;
-    //value;
+    next;
+    row;
+    value;
 
     constructor(row, value) {
         this.row = row;
@@ -28,7 +26,7 @@ class NodeRow {
 }
 
 class ListCol {
-    //head;
+    head;
 
     constructor() {
         this.head = null;
@@ -37,35 +35,29 @@ class ListCol {
     issetCol(col) {
         var temp = this.head;
         while (temp !== null) {
-
             if (temp.col === col) {
                 return true;
             }
-
             temp = temp.next;
         }
         return false;
     }
 
     insertCol(col) {
-        
         var newNode = new NodeCol(col);
         if (!this.issetCol(col)) {
-            
             if (this.head !== null) {//si la cabeza esta nula
                 var temp = this.head;
                 //alert("Entro");
-
-                if (parseFloat(newNode.col)>=parseFloat(temp.col)) {//si el primero en la lista es menor que el nuevo nodo
+                if (parseFloat(newNode.col) >= parseFloat(temp.col)) {//si el primero en la lista es menor que el nuevo nodo
                     while (temp.next !== null) {
-                        if (parseFloat(newNode.col)>=parseFloat(temp.next.col)) {
+                        if (parseFloat(newNode.col) >= parseFloat(temp.next.col)) {
                             temp = temp.next;
                         } else {
                             break;
                         }
                     }
-
-                    if (newNode.next !== null && parseFloat(newNode.next.col)>=parseFloat(temp.col)) {//si temp es el ultimo en la cola revisar si es menor (el bucle no diferencia si el ultimo es menor porque es el ultimo, no puede devolver null)
+                    if (newNode.next !== null && parseFloat(newNode.next.col) >= parseFloat(temp.col)) {//si temp es el ultimo en la cola revisar si es menor (el bucle no diferencia si el ultimo es menor porque es el ultimo, no puede devolver null)
                         newNode.next = null;
                         temp.next = newNode;
                     } else {//si esta en medio del primero y el ultimo
@@ -85,11 +77,9 @@ class ListCol {
     }
 
     insertRow(col, row, value) {
-
         if (this.issetCol(col)) {
             var temp = this.head;
             while (temp !== null) {
-
                 if (temp.col === col) {
                     temp.down.insertRow(row, value);
                     //console.log("cabeza down");
@@ -97,21 +87,17 @@ class ListCol {
                     if (temp.down.head === null) {
                         this.deleteCol(col);
                     }
-
                     break;
                 }
-
                 temp = temp.next;
             }
         }
     }
 
     sumRow(col, row, value) {
-
         if (this.issetCol(col)) {
             var temp = this.head;
             while (temp !== null) {
-
                 if (temp.col === col) {
                     temp.down.sumRow(row, value);
                     //console.log("cabeza down");
@@ -119,26 +105,21 @@ class ListCol {
                     if (temp.down.head === null) {
                         this.deleteCol(col);
                     }
-
                     break;
                 }
-
                 temp = temp.next;
             }
         }
     }
 
     deleteRow(col, row) {
-
         if (this.issetCol(col)) {
             var temp = this.head;
             while (temp !== null) {
-
                 if (temp.col === col) {
                     temp.down.deleteRow(row);
                     break;
                 }
-
                 temp = temp.next;
             }
         } else {
@@ -147,15 +128,11 @@ class ListCol {
     }
 
     deleteCol(col) {
-
         var temp = this.head;
         if (temp.col !== col) {
-
             while (temp.next !== null) {
                 if (temp.next.col === col) {//si el siguiente es el q buscamos
-
                     if (temp.next.down.head === null) {//si esta vacio
-
                         if (temp.next.next === null) {//es el ultimo
                             temp.next = null;
                         } else {//esta en medio de la lista y temp es el anterior
@@ -163,13 +140,11 @@ class ListCol {
                             temp.next = removed.next;
                             removed.next = null;
                         }
-
                     } else {
                         alert("No se puede retirar la materia " + col + ", tiene estudiantes matriculados");
                     }
                     break;
                 }
-
                 temp = temp.next;
             }
         } else {//es el primero en la cola
@@ -194,7 +169,6 @@ class ListCol {
     print() {
         var temp = this.head;
         while (temp !== null) {
-
             console.log(temp.col);
             temp = temp.next;
             console.log("alumnos:");
@@ -206,20 +180,17 @@ class ListCol {
         var temp = this.head;
         var max = 0;
         while (temp !== null) {
-
             if (temp.down.countRows() > max) {
                 max = temp.down.countRows();
             }
-
             temp = temp.next;
         }
-
         return max;
     }
 }
 
 class ListRow {
-    //head;
+    head;
 
     constructor() {
         this.head = null;
@@ -228,11 +199,9 @@ class ListRow {
     issetRow(row) {
         var temp = this.head;
         while (temp !== null) {
-
             if (temp.row === row) {
                 return true;
             }
-
             temp = temp.next;
         }
         return false;
@@ -241,11 +210,9 @@ class ListRow {
     getRow(row) {
         var temp = this.head;
         while (temp !== null) {
-
             if (temp.row === row) {
                 return temp;
             }
-
             temp = temp.next;
         }
         return null;
@@ -256,22 +223,18 @@ class ListRow {
         if (this.issetRow(row)) {
             this.deleteRow(row);
         }
-
         if (parseFloat(value) !== 0) {
-            //alert("valor dif a 0 = "+value);
-
             if (this.head !== null) {//si la cabeza esta nula
                 var temp = this.head;
-                if (parseFloat(newNode.row)>=parseFloat(temp.row)) {//si el primero en la lista es menor que el nuevo nodo
+                if (parseFloat(newNode.row) >= parseFloat(temp.row)) {//si el primero en la lista es menor que el nuevo nodo
                     while (temp.next !== null) {//mientras encuentre mas menores
-                        if (parseFloat(newNode.row)>=parseFloat(temp.next.row)) {
+                        if (parseFloat(newNode.row) >= parseFloat(temp.next.row)) {
                             temp = temp.next;
                         } else {
                             break;
                         }
                     }
-
-                    if (newNode.next !== null && parseFloat(newNode.next.row)>=parseFloat(temp.row)) {//si temp es el ultimo en la cola revisar si es menor (el bucle no diferencia si el ultimo es menor porque es el ultimo, no puede devolver null)
+                    if (newNode.next !== null && parseFloat(newNode.next.row) >= parseFloat(temp.row)) {//si temp es el ultimo en la cola revisar si es menor (el bucle no diferencia si el ultimo es menor porque es el ultimo, no puede devolver null)
                         newNode.next = null;
                         temp.next = newNode;
                     } else {//si esta en medio del primero y el ultimo
@@ -279,116 +242,17 @@ class ListRow {
                         temp.next = newNode;
                     }
                 } else {//si va antes del primero+
-
-                        var tempo = this.head;
-                        this.head = newNode;
-                        this.head.next = tempo;
-                       
-
-                    
-                   /*if(this.head>newNode)
-                    {
-                        var tempo = this.head;
-                        this.head = newNode;
-                        this.head.next = tempo;
-                    }
-                    else
-                    {
-                        var tempo = this.head;
-                        this.head = newNode;
-                        this.head.next = tempo;
-                    }
-
-                    /*if((this.head.compareTo(newNode))<0)
-                    {
-                        var tempo = this.head;
-                        this.head = newNode;
-                        this.head.next = tempo;
-                    }
-                    else 
-
-                    {
-                        alert("entro");
-                        var tempo = this.head;
-                        this.head = newNode;
-                        newNode.next = tempo;
-                    }
-                    */
+                    var tempo = this.head;
+                    this.head = newNode;
+                    this.head.next = tempo;
                 }
             } else {//si aun no existe lista
                 this.head = newNode;
             }
         }
-
-        /*if (parseFloat(value) !== 0) {
-            //alert("valor dif a 0 = "+value);
-
-            if (this.head !== null) {//si la cabeza esta nula
-                var temp = this.head;
-                if (newNode.row.localeCompare(temp.row) > -1) {//si el primero en la lista es menor que el nuevo nodo
-                    while (temp.next !== null) {//mientras encuentre mas menores
-                        if (newNode.row.localeCompare(temp.next.row) > -1) {
-                            temp = temp.next;
-                        } else {
-                            break;
-                        }
-                    }
-
-                    if (newNode.next !== null && newNode.next.row.localeCompare(temp.row) > -1) {//si temp es el ultimo en la cola revisar si es menor (el bucle no diferencia si el ultimo es menor porque es el ultimo, no puede devolver null)
-                        newNode.next = null;
-                        temp.next = newNode;
-                    } else {//si esta en medio del primero y el ultimo
-                        newNode.next = temp.next;
-                        temp.next = newNode;
-                    }
-                } else {//si va antes del primero+
-
-                        var tempo = this.head;
-                        this.head = newNode;
-                        this.head.next = tempo;
-                       
-
-                    
-                   /*if(this.head>newNode)
-                    {
-                        var tempo = this.head;
-                        this.head = newNode;
-                        this.head.next = tempo;
-                    }
-                    else
-                    {
-                        var tempo = this.head;
-                        this.head = newNode;
-                        this.head.next = tempo;
-                    }
-
-                    /*if((this.head.compareTo(newNode))<0)
-                    {
-                        var tempo = this.head;
-                        this.head = newNode;
-                        this.head.next = tempo;
-                    }
-                    else 
-
-                    {
-                        alert("entro");
-                        var tempo = this.head;
-                        this.head = newNode;
-                        newNode.next = tempo;
-                    }
-                    
-                }
-            } else {//si aun no existe lista
-                this.head = newNode;
-            }
-        }*/
-
-
-
     }
 
     sumRow(row, value) {
-
         if (value !== 0) {
             var newNode = new NodeRow(row, value);
             if (this.issetRow(row)) {
@@ -398,7 +262,6 @@ class ListRow {
                 if (value !== 0) {
                     this.insertRow(row, value);
                 }
-
             } else {
                 this.insertRow(row, value);
             }
@@ -406,10 +269,8 @@ class ListRow {
     }
 
     deleteRow(row) {
-
         var temp = this.head;
         if (temp.row !== row) {
-
             while (temp.next !== null) {
                 if (temp.next.row === row) {//si el siguiente es el q buscamos
                     if (temp.next.next === null) {//es el ultimo
@@ -419,10 +280,8 @@ class ListRow {
                         temp.next = removed.next;
                         removed.next = null;
                     }
-
                     break;
                 }
-
                 temp = temp.next;
             }
         } else {//es el primero en la cola
@@ -443,7 +302,6 @@ class ListRow {
     print() {
         var temp = this.head;
         while (temp !== null) {
-
             console.log(temp.row);
             temp = temp.next;
         }
@@ -457,7 +315,6 @@ class ListRow {
             counter++;
             temp = temp.next;
         }
-
         return counter;
     }
 }
@@ -465,87 +322,64 @@ class ListRow {
 //FUNCIONES
 function printCols(matrix) {
     if (matrix === 'A') {
-        matrixList = matrixListA;
+        var matrixList = matrixListA;
     } else if (matrix === 'B') {
-        matrixList = matrixListB;
+        var matrixList = matrixListB;
     } else {
-        matrixList = matrixListC;
+        var matrixList = matrixListC;
     }
 
     var headNode = matrixList.head;
     var counter = 0;
     $("#tr_cols" + matrix).html("");
-
     while (headNode !== null) {
-
         if (counter > 0) {
             $("#tr_cols" + matrix).append('<td id="th_arrow_1"><img src="img/arrow-right.png" alt="right-arrow" width="29"></td>');
         }
-
         $("#tr_cols" + matrix).append('<td id="th_col_1"><table class="table-responsive" style="text-align: center"><tr><td>' + headNode.col + '</td>' + (headNode.next === null ? '<td><strong>.</strong></td>' : '<td colspan="2" class="p-1"></td>') + '</tr><tr><td colspan="2" class="p-1"></td></tr></table></td>');
-
         headNode = headNode.next;
         counter++;
     }
 }
 
 function printRows(matrix) {
-
     if (matrix === 'A') {
-        matrixList = matrixListA;
+        var matrixList = matrixListA;
     } else if (matrix === 'B') {
-        matrixList = matrixListB;
+        var matrixList = matrixListB;
     } else {
-        matrixList = matrixListC;
+        var matrixList = matrixListC;
     }
 
     var headNode = matrixList.head;
     var quantMaxRows = matrixList.getMaxCountRows();
-
     var stringHtml = "";
-
     $('#t_rows' + matrix).html("");
-
     for (var i = 0; i < quantMaxRows; i++) {
         stringHtml +=
                 '<tr id="tr_arrow_' + i + matrix + '"></tr><tr id="tr_badge_' + i + matrix + '"></tr>';
     }
 
     $('#t_rows' + matrix).append(stringHtml);
-
     var counterCol = 0;
     while (headNode !== null) {
-
         var headNodeRow = headNode.down.head;
-
         var counterRow = 0;
-
         for (var j = 0; j < quantMaxRows; j++) {
-
-            if (counterCol > 0) {//AÃ±adir espacio a la izquierda debajo de la flecha de materia
+            if (counterCol > 0) {//Añadir espacio a la izquierda debajo de la flecha de materia
                 $('#tr_arrow_' + counterRow + matrix).append('<td class="td_row_' + counterCol + '"></td>');
-
                 $('#tr_badge_' + counterRow + matrix).append('<td class="td_row_' + counterCol + '"></td>');
             }
-
             if (headNodeRow !== null) {
                 $('#tr_arrow_' + counterRow + matrix).append('<td class="td_row_' + counterCol + '"><img src="img/arrow-down.png" alt="down-arrow" width="30"></td>');
-
-
-
                 $('#tr_badge_' + counterRow + matrix).append('<td class="td_row_' + counterCol + '"><table class="table-responsive" style="text-align: center"><tr><td>' + headNodeRow.row + '</td><td><strong>' + headNodeRow.value + '</strong></td></tr><tr>' + (headNodeRow.next === null ? '<td colspan="2"><strong>.</strong></td>' : '<td colspan="2" class="p-1"></td>') + '</tr></table></td>');
-
                 headNodeRow = headNodeRow.next;
             } else {
                 $('#tr_arrow_' + counterRow + matrix).append('<td class="td_row_' + counterCol + '"></td>');
-
-
                 $('#tr_badge_' + counterRow + matrix).append('<td class="td_row_' + counterCol + '"></td>');
             }
-
             counterRow++;
         }
-
         counterCol++;
         headNode = headNode.next;
     }
@@ -553,13 +387,11 @@ function printRows(matrix) {
 
 function insertRow(matrix) {
     if (matrix === 'A') {
-        matrixList = matrixListA;
+        var matrixList = matrixListA;
     } else if (matrix === 'B') {
-        matrixList = matrixListB;
+        var matrixList = matrixListB;
     }
-
     if ($("#colTxt" + matrix).val() !== "" && $("#rowTxt" + matrix).val() !== "" && $("#valueTxt" + matrix).val() !== "") {
-
         matrixList.insertCol($("#colTxt" + matrix).val());
         matrixList.insertRow($("#colTxt" + matrix).val(), $("#rowTxt" + matrix).val(), $("#valueTxt" + matrix).val());
         $("#colTxt" + matrix).val("");
@@ -577,7 +409,6 @@ function insertRow(matrix) {
 
 function sum() {
     matrixListC.head = null;
-
     var colA = matrixListA.head;
     while (colA !== null) {
         var rowA = colA.down.head;
@@ -586,10 +417,8 @@ function sum() {
             matrixListC.sumRow(colA.col, rowA.row, rowA.value);
             rowA = rowA.next;
         }
-
         colA = colA.next;
     }
-
     var colB = matrixListB.head;
     while (colB !== null) {
         var rowB = colB.down.head;
@@ -598,12 +427,9 @@ function sum() {
             matrixListC.sumRow(colB.col, rowB.row, rowB.value);
             rowB = rowB.next;
         }
-
         colB = colB.next;
     }
-
     console.log(matrixListC.head);
-
     printCols('C');
     printRows('C')
 }
